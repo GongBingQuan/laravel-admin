64aec36c42158ced1bad7acb74ebd74a5c71717b
zzjgbq <zzjgbq@qq.com>
2020-11-04 11:36:21 +0800

    生成.env

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-sed -i 's/{APP_KEY}/${APP_KEY}/' /usr/share/nginx/htm/.env.example
-sed -i 's/{DB_HOST}/${DB_HOST}/' /usr/share/nginx/htm/.env.example
-sed -i 's/{DB_PASSWORD}/${DB_PASSWORD}/' /usr/share/nginx/htm/.env.example
+sed -i 's/{APP_KEY}/'${APP_KEY}'/' ./.env.example
+sed -i 's/{DB_HOST}/'${DB_HOST}'/' ./.env.example
+sed -i 's/{DB_PASSWORD}/'${DB_PASSWORD}'/' ./.env.example
