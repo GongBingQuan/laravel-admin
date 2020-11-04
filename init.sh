7cde6006ddbd08eb76d260234b9110f6fef9c28e
zzjgbq <zzjgbq@qq.com>
2020-11-04 13:32:04 +0800

    权限

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chmod 777 /usr/share/nginx/html
-if [ ! -f "./env" ];then
+chmod 777 ./
+if [ ! -f "./.env" ];then
   echo ".env不存在"
   sed -i 's/{APP_KEY}/'$APP_KEY'/' ./.env.example
   sed -i 's/{DB_HOST}/'$DB_HOST'/' ./.env.example
