c5a1464dcbfa892cda0771b98beab0be89990a2e
zzjgbq <zzjgbq@qq.com>
2020-11-04 11:05:45 +0800

    生成.env测试

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed -i 's/原字符串/新字符串/' /home/1.txt
+sed -i 's/{APP_KEY}/${APP_KEY}/' /usr/share/nginx/htm/.env.example
+sed -i 's/{DB_HOST}/${DB_HOST}/' /usr/share/nginx/htm/.env.example
+sed -i 's/{DB_PASSWORD}/${DB_PASSWORD}/' /usr/share/nginx/htm/.env.example
