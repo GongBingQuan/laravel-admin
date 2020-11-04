e1c53b1898704f864d7ebf8fbcf99bba2afe39aa
zzjgbq <zzjgbq@qq.com>
2020-11-04 12:44:18 +0800

    启动命令修改

--- init.sh
+++ init.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-set -e
-supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
-
-/usr/sbin/crond   -f  -L  /var/log/cron/cron.log
 
 if [ ! -f "./env" ];then
   echo ".env不存在"
-  if [ -z $DB_PASSWORD ]; then
-    sed -i 's/{APP_KEY}/'$APP_KEY'/' ./.env.example
-    sed -i 's/{DB_HOST}/'$DB_HOST'/' ./.env.example
-    sed -i 's/{DB_PASSWORD}/'$DB_PASSWORD'/' ./.env.example
-    
-  else
-    echo ".环境变量DB_PASSWORD不存在"
-  fi
-fi
-if [ ! -f "./vendor" ];then
+  sed -i 's/{APP_KEY}/'$APP_KEY'/' ./.env.example
+  sed -i 's/{DB_HOST}/'$DB_HOST'/' ./.env.example
+  sed -i 's/{DB_PASSWORD}/'$DB_PASSWORD'/' ./.env.example
+  mv .env.example .env
   composer install
 fi
