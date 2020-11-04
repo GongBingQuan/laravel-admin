0d8419dd8a4d0d51237d38e27df1170c1b59c235
zzjgbq <zzjgbq@qq.com>
2020-11-04 11:44:38 +0800

    输出提示

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@
 sed -i 's/{APP_KEY}/'${APP_KEY}'/' ./.env.example
 sed -i 's/{DB_HOST}/'${DB_HOST}'/' ./.env.example
 sed -i 's/{DB_PASSWORD}/'${DB_PASSWORD}'/' ./.env.example
+
+echo ${APP_KEY}'+'${DB_HOST}'+'${DB_PASSWORD}
