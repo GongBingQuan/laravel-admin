3d0692d91701ae1117bb2074c2f76c17a0967f00
zzjgbq <zzjgbq@qq.com>
2020-11-04 13:17:40 +0800

    测试

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
+supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
+
+/usr/sbin/crond   -f  -L  /var/log/cron/cron.log
+
+#exec redis-server --requirepass develop
 
 if [ ! -f "./env" ];then
   echo ".env不存在"
