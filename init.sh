16f5ee2bed09620f6b9a66f8145bf145cbf80d79
zzjgbq <zzjgbq@qq.com>
2020-11-04 11:48:28 +0800

    bug修复

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i 's/{APP_KEY}/'${APP_KEY}'/' ./.env.example
-sed -i 's/{DB_HOST}/'${DB_HOST}'/' ./.env.example
-sed -i 's/{DB_PASSWORD}/'${DB_PASSWORD}'/' ./.env.example
+sed -i 's/{APP_KEY}/'$APP_KEY'/' ./.env.example
+sed -i 's/{DB_HOST}/'$DB_HOST'/' ./.env.example
+sed -i 's/{DB_PASSWORD}/'$DB_PASSWORD'/' ./.env.example
 
-echo ${APP_KEY}'+'${DB_HOST}'+'${DB_PASSWORD}
+echo $APP_KEY'+'$DB_HOST'+'$DB_PASSWORD
